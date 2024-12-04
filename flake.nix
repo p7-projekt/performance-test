38e91ce8db0cd5f98af561e4bdf247aa2e49caae
Andreas Krath <andreas.krath+github@gmail.com>
2024-12-04 19:59:55 +0100

    dev: add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development environment for working with k6.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              k6
+              typescript-language-server
+            ];
+          };
+      }
+    );
+}

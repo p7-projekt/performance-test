d0a64df8ed39cf93c5384ecc5444335760bb2002
Andreas Krath <andreas.krath+github@gmail.com>
2024-12-05 13:52:25 +0100

    tests: add smoke test

---
+++ tests/smoke.js
@@ -0,0 +1,33 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { getExercise } from "../exercises/exercise";
+import { generateSetup } from "../util/setup";
+
+export const options = {
+	stages: [
+		{ duration: "1m", target: 200 },
+		{ duration: "5m", target: 200 },
+		{ duration: "1m", target: 0 },
+	],
+};
+
+const data = new SharedArray("users", generateSetup(200, 2));
+
+export default function () {
+	const user = data[__VU];
+	let exercise = getExercise();
+	const url = `http://localhost:80/v2/exercises/${exercise.payload.exerciseId}/submission`;
+	const params = {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${user.userId}`,
+		},
+	};
+	delete exercise.payload.exerciseId;
+	exercise.payload.sessionId = user.sessionId;
+
+	const res = http.post(url, JSON.stringify(exercise.payload), params);
+	console.log("body: " + res.body);
+	check(res, solution.check);
+	sleep(30);
+}

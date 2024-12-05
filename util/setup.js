139b2a7a7a6647ac2f450ccd9998d01ae8e4b91b
Andreas Krath <andreas.krath+github@gmail.com>
2024-12-05 14:27:31 +0100

    util: fix module imports

--- util/setup.js
+++ util/setup.js
@@ -1,22 +1,29 @@
-const { login, createRandomUsers } = require('./generateUsers');
-const { generateRandomClassrooms } = require('./generateClassrooms');
-const { createSessionsForAllClassrooms, createExercises } = require('./generateSessions');
-const { distributeStudentsToSessions } = require('./distributeStudents');
-const { getExercises } = require('./generateExercise')
+import { login, createRandomUsers } from "./generateUsers.js";
+import { generateRandomClassrooms } from "./generateClassrooms.js";
+import {
+	createSessionsForAllClassrooms,
+	createExercises,
+} from "./generateSessions.js";
+import { distributeStudentsToSessions } from "./distributeStudents.js";
+import { getExercises } from "./generateExercise.js";
 
-export async function generateSetup(numberOfUsers, numberOfClassrooms){
-    token = await login("admin@p7.dk", "Admin!1234");
-    let userIds = []
-    userIds = await createRandomUsers(numberOfUsers);
-    console.log("User ids collected");
-    //Insert exercises
-    const exercises = getExercises();
-    await createExercises(exercises, token);
-    //Generate classrooms
-    const classRoomIds = await generateRandomClassrooms(numberOfClassrooms, token)
-    console.log("ClassRooms created")
-    //Generate sessions on classrooms
-    await createSessionsForAllClassrooms(classRoomIds, token);
-    console.log("Sessions in classrooms created");
-    return await distributeStudentsToSessions(userIds, classRoomIds, token);
+export async function generateSetup(numberOfUsers, numberOfClassrooms) {
+	token = await login("admin@p7.dk", "Admin!1234");
+	let userIds = [];
+	userIds = await createRandomUsers(numberOfUsers);
+	console.log("User ids collected");
+	//Insert exercises
+	const exercises = getExercises();
+	await createExercises(exercises, token);
+	//Generate classrooms
+	const classRoomIds = await generateRandomClassrooms(
+		numberOfClassrooms,
+		token,
+	);
+	console.log("ClassRooms created");
+	//Generate sessions on classrooms
+	await createSessionsForAllClassrooms(classRoomIds, token);
+	console.log("Sessions in classrooms created");
+	return await distributeStudentsToSessions(userIds, classRoomIds, token);
 }
+

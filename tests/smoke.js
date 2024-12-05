cf7c539660d072d3fd604d825b5a9d115e55f5ac
Schoogle <Jona193j@gmail.com>
2024-12-05 17:45:32 +0100

    fixed await issues

--- tests/smoke.js
+++ tests/smoke.js
@@ -19,7 +19,7 @@ export function setup() {
 export default function (data) {
 	const user = data[__VU];
 	let exercise = getExercise();
-	console.log(`HERE:${user}, ${exercise}`);
+	console.log(`HERE:${user.userId}, ${exercise.payload.exerciseId}`);
 	const url = `http://localhost:80/v2/exercises/${exercise.payload.exerciseId}/submission`;
 	const params = {
 		headers: {

95e96fb288039849dab80827bb84aaf21da7d5d6
Schoogle <Jona193j@gmail.com>
2024-12-07 12:29:03 +0100

    added test for test ing haskell values

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -9,7 +9,7 @@ import { areaExercise } from "./area.js";
 import { equalStringsExercise } from "./equalStrings.js";
 import { signDetectorExercise } from "./signDetector.js";
 
-export const HASKELL_EXERCISE_RATE = 0.3;
+export const HASKELL_EXERCISE_RATE = 0.2;
 
 export function newHaskellSolution(solution, check, exerciseId) {
 	return {
---
+++ tests/value_adjust.js
@@ -0,0 +1,37 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { getExercise } from "../exercises/exercise.js";
+import { SharedArray } from "k6/data";
+
+export const options = {
+	setupTimeout: "30m",
+	stages: [
+		{ duration: "10m", target: 4000 },
+		{ duration: "5m", target: 4000 },
+		{ duration: "1m", target: 0 },
+	],
+};
+
+const data = new SharedArray("some name", function () {
+	const f = JSON.parse(open("./users.json"));
+	return f;
+});
+
+export default function () {
+	const user = data[__VU - 1];
+	let exercise = getExercise();
+	const url = `http://localhost:80/v2/exercises/${exercise.payload.exerciseId}/submission`;
+	const params = {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${user.userId}`,
+		},
+	};
+	delete exercise.payload.exerciseId;
+	exercise.payload.sessionId = user.sessionId;
+	const res = http.post(url, JSON.stringify(exercise.payload), params);
+	if (!check(res, exercise.check)) {
+		console.error("FAILED CHECK");
+	}
+	sleep(30);
+}
